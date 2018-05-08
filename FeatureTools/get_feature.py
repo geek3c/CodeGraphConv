cad8f0897d9bd642c5813281d631eacf4e8a6534
my <mayuanucas@qq.com>
2018-05-08 21:10:58 +0800

    基本块特征提取

---
+++ FeatureTools/get_feature.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+from utils import generate_i64
+
+
+def get_one_function_feature():
+    pass
+
+
+def get_all_function_feature(binary_path):
+    i64_filepath = "./myi64"
+    i64_path = generate_i64(binary_path, i64_filepath)
+    print i64_path
+
+
+def help():
+    print "使用方法:"
+    print "python %s 二进制程序路径" % sys.argv[0]
+
+
+if __name__ == "__main__":
+    print "从二进制程序中提取基本块层次的特征信息"
+
+    if len(sys.argv) < 3:
+        help()
+        exit(-1)
+
+    print "step 1: 生成二进制程序对应的i64文件"
+    get_all_function_feature(sys.argv[1])
